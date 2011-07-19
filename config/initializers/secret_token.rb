35f90aef20bc29a42392bd472dd058d75dcf3f79
root <root@troya-studio.com>
2011-07-19 20:47:39 +0300

    firs commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'afdb86e40ee0444ac46751ac6d0384e4c99b3ae91d59f085e28553e2e322feadbeb46016029fd3ef48cb4bd75d319b4e74873b9d9afa2aaf9c85dc617e3b87d8'
